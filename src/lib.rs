2871f92e08797c03e74f5c4e64db4f6528ce8444
Jon Gjengset <jon@thesquareplanet.com>
2020-09-03 10:57:57 -0400

    New lint name

--- src/lib.rs
+++ src/lib.rs
@@ -106,7 +106,7 @@
 #![warn(
     missing_docs,
     missing_debug_implementations,
-    intra_doc_link_resolution_failure,
+    broken_intra_doc_links,
     rust_2018_idioms,
     unreachable_pub
 )]
