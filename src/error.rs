f24da771288241102640bdb3e3c888083bf5050b
Jiahao XU <Jiahao_XU@outlook.com>
2021-10-14 20:16:04 +1100

    Simplify impl of Error::interpret_ssh_error
    As suggested by `cargo clippy`.
    
    The new impl requires rustc to be >= 1.52.0, and the MSRV is 1.52.1.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/error.rs
+++ src/error.rs
@@ -80,7 +80,7 @@ impl Error {
         }
         if stderr.starts_with("Warning: Permanently added ") {
             // added to hosts file -- let's ignore that message
-            stderr = stderr.splitn(2, "\r\n").nth(1).unwrap_or("");
+            stderr = stderr.split_once("\r\n").map(|x| x.1).unwrap_or("");
         }
         let mut kind = io::ErrorKind::ConnectionAborted;
         let mut err = stderr.splitn(2, ": ");
