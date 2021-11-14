f65cdd6efaa04d2d6c5ccb562d6c4bffeaa8611b
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-14 22:56:46 +1100

    Uncomment remote_socket_forward and local_socket_forward
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- tests/openssh.rs
+++ tests/openssh.rs
@@ -1,6 +1,12 @@
 use lazy_static::lazy_static;
 use std::io;
 use std::io::Write;
+use std::net::{IpAddr, SocketAddr};
+
+use core::time::Duration;
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio::time::sleep;
 
 use regex::Regex;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -11,6 +17,10 @@ use openssh::*;
 
 fn addr() -> String {
     std::env::var("TEST_HOST").unwrap_or("ssh://test-user@127.0.0.1:2222".to_string())
+}
+
+fn loopback() -> IpAddr {
+    "127.0.0.1".parse().unwrap()
 }
 
 #[cfg(not(feature = "mux_client"))]
@@ -806,120 +816,102 @@ async fn auth_failed() {
     }
 }
 
-//use core::num::NonZeroU32;
-//use core::time::Duration;
+#[tokio::test]
+#[cfg_attr(not(ci), ignore)]
+async fn remote_socket_forward() {
+    let sessions = connects().await;
+    for (session, ports) in sessions.iter().zip(&[(9999, 1234), (9998, 1233)]) {
+        let output_listener = TcpListener::bind(("127.0.0.1", ports.0)).await.unwrap();
 
-//use std::fs;
-//use tokio::net::{TcpListener, TcpStream};
-//use tokio::time::sleep;
+        eprintln!("Requesting port forward");
+        session
+            .request_port_forward(
+                ForwardType::Remote,
+                Socket::TcpSocket(SocketAddr::new(loopback(), ports.1)),
+                Socket::TcpSocket(SocketAddr::new(loopback(), ports.0)),
+            )
+            .await
+            .unwrap();
 
-// These two tests are commented out because linuxserver/openssh-server does not
-// allow port forwarding.
-//
-// However, these functionalities are tested in openssh_mux_client, so I suppose they
-// are actually ready to use.
-//
-//#[tokio::test]
-//async fn remote_socket_forward() {
-//    let session = Session::connect_mux(&addr(), KnownHosts::Accept).await.unwrap();
-//
-//    let output_listener = TcpListener::bind(("127.0.0.1", 1234)).await.unwrap();
-//
-//    eprintln!("Requesting port forward");
-//    session
-//        .request_port_forward(
-//            ForwardType::Remote,
-//            &Socket::TcpSocket {
-//                port: NonZeroU32::new(9999).unwrap(),
-//                host: "127.0.0.1",
-//            },
-//            &Socket::TcpSocket {
-//                port: NonZeroU32::new(1234).unwrap(),
-//                host: "127.0.0.1",
-//            },
-//        )
-//        .await
-//        .expect(str::from_utf8(&read_log(&session)).unwrap());
-//
-//    eprintln!("Creating remote process");
-//    let cmd = "echo -e '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n' | nc localhost 9999 >/dev/stderr";
-//    let child = session
-//        .raw_command(cmd)
-//        .stderr(Stdio::piped())
-//        .spawn()
-//        .await
-//        .unwrap();
-//
-//    eprintln!("Waiting for connection");
-//    let (mut output, _addr) = output_listener.accept().await.unwrap();
-//
-//    eprintln!("Reading");
-//
-//    const DATA: &[u8] = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n".as_bytes();
-//
-//    let mut buffer = [0 as u8; DATA.len()];
-//    output.read_exact(&mut buffer).await.unwrap();
-//
-//    assert_eq!(DATA, &buffer);
-//
-//    drop(output);
-//    drop(output_listener);
-//
-//    eprintln!("Waiting for session to end");
-//    let output = child.wait_with_output().await.unwrap();
-//    eprintln!("remote_socket_forward: {:#?}", output);
-//    assert!(output.status.success());
-//}
-//
-//#[tokio::test]
-//async fn local_socket_forward() {
-//    let session = Session::connect_mux(&addr(), KnownHosts::Accept).await.unwrap();
-//
-//    eprintln!("Creating remote process");
-//    let cmd = "echo -e '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n' | nc -l -p 1433 >/dev/stderr";
-//    let child = session
-//        .raw_command(cmd)
-//        .stderr(Stdio::piped())
-//        .spawn()
-//        .await
-//        .unwrap();
-//
-//    sleep(Duration::from_secs(1)).await;
-//
-//    eprintln!("Requesting port forward");
-//    session
-//        .request_port_forward(
-//            ForwardType::Local,
-//            &Socket::TcpSocket {
-//                port: NonZeroU32::new(1235).unwrap(),
-//                host: "127.0.0.1",
-//            },
-//            &Socket::TcpSocket {
-//                port: NonZeroU32::new(1433).unwrap(),
-//                host: "127.0.0.1",
-//            },
-//        )
-//        .await
-//        .unwrap();
-//
-//    eprintln!("Connecting to forwarded socket");
-//    let mut output = TcpStream::connect(("127.0.0.1", 1235)).await.unwrap();
-//
-//    eprintln!("Reading");
-//
-//    const DATA: &[u8] = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n".as_bytes();
-//    let mut buffer = [0 as u8; DATA.len()];
-//    output
-//        .read_exact(&mut buffer)
-//        .await
-//        .expect(str::from_utf8(&read_log(&session)).unwrap());
-//
-//    assert_eq!(DATA, buffer);
-//
-//    drop(output);
-//
-//    eprintln!("Waiting for session to end");
-//    let output = child.wait_with_output().await.unwrap();
-//    eprintln!("local_socket_forward: {:#?}", output);
-//    assert!(output.status.success());
-//}
+        eprintln!("Creating remote process");
+        let cmd = format!(
+            "echo -e '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n' | nc localhost {} >/dev/stderr",
+            ports.1
+        );
+        let child = session
+            .raw_command(cmd)
+            .stderr(Stdio::piped())
+            .spawn()
+            .await
+            .unwrap();
+
+        eprintln!("Waiting for connection");
+        let (mut output, _addr) = output_listener.accept().await.unwrap();
+
+        eprintln!("Reading");
+
+        const DATA: &[u8] = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n".as_bytes();
+
+        let mut buffer = [0 as u8; DATA.len()];
+        output.read_exact(&mut buffer).await.unwrap();
+
+        assert_eq!(DATA, &buffer);
+
+        drop(output);
+        drop(output_listener);
+
+        eprintln!("Waiting for session to end");
+        let output = child.wait_with_output().await.unwrap();
+        eprintln!("remote_socket_forward: {:#?}", output);
+        assert!(output.status.success());
+    }
+}
+
+#[tokio::test]
+#[cfg_attr(not(ci), ignore)]
+async fn local_socket_forward() {
+    let sessions = connects().await;
+    for (session, ports) in sessions.iter().zip([(1235, 1433), (1236, 1432)]) {
+        eprintln!("Creating remote process");
+        let cmd = format!(
+            "echo -e '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n' | nc -l -p {} >/dev/stderr",
+            ports.1
+        );
+        let child = session
+            .raw_command(cmd)
+            .stderr(Stdio::piped())
+            .spawn()
+            .await
+            .unwrap();
+
+        sleep(Duration::from_secs(1)).await;
+
+        eprintln!("Requesting port forward");
+        session
+            .request_port_forward(
+                ForwardType::Local,
+                Socket::TcpSocket(SocketAddr::new(loopback(), ports.0)),
+                Socket::TcpSocket(SocketAddr::new(loopback(), ports.1)),
+            )
+            .await
+            .unwrap();
+
+        eprintln!("Connecting to forwarded socket");
+        let mut output = TcpStream::connect(("127.0.0.1", ports.0)).await.unwrap();
+
+        eprintln!("Reading");
+
+        const DATA: &[u8] = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n".as_bytes();
+        let mut buffer = [0 as u8; DATA.len()];
+        output.read_exact(&mut buffer).await.unwrap();
+
+        assert_eq!(DATA, buffer);
+
+        drop(output);
+
+        eprintln!("Waiting for session to end");
+        let output = child.wait_with_output().await.unwrap();
+        eprintln!("local_socket_forward: {:#?}", output);
+        assert!(output.status.success());
+    }
+}
