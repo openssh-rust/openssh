13535e73449708a26166bc5913fac1623295898c
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-14 22:56:16 +1100

    Optimize run_ci_tests.sh: Run `cargo build` after starting the sshd
    So that it has enough time to install the dep required.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -2,9 +2,6 @@
 
 cd $(dirname `realpath $0`)
 
-export RUSTFLAGS='--cfg=ci'
-cargo build --all-features --tests
-
 # Start the container
 export PUBLIC_KEY='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGzHvK2pKtSlZXP9tPYOOBb/xn0IiC9iLMS355AYUPC7'
 export DOCKER_MODS='linuxserver/mods:openssh-server-ssh-tunnel'
@@ -21,8 +18,10 @@ docker run \
     -e PUBLIC_KEY \
     linuxserver/openssh-server:amd64-latest
 
+RUSTFLAGS='--cfg=ci' cargo build --all-features --tests
+
 # Wait for docker mod to be installed and sshd starts up
-sleep 30
+sleep 25
 
 function cleanup {
     docker stop $name
