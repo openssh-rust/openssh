2c9b410d37a4b282f91a6a4fc59f565b86f4957e
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-22 17:23:11 +1100

    Add new example native-mux_tsp.rs
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ examples/native-mux_tsp.rs
@@ -0,0 +1,19 @@
+use openssh::*;
+
+#[tokio::main]
+async fn main() {
+    let session = Session::connect_mux("ssh://jon@ssh.thesquareplanet.com:222", KnownHosts::Strict)
+        .await
+        .unwrap();
+
+    let ls = session.command("ls").output().await.unwrap();
+    eprintln!(
+        "{}",
+        String::from_utf8(ls.stdout).expect("server output was not valid UTF-8")
+    );
+
+    let whoami = session.command("whoami").output().await.unwrap();
+    assert_eq!(whoami.stdout, b"jon\n");
+
+    session.close().await.unwrap();
+}
