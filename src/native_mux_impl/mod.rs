a0d6ca7bb716644a365d167a449089a8bfd25744
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-25 01:15:08 +1100

    Refactor native_mux_impl: Extract out module session
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/native_mux_impl/mod.rs
+++ src/native_mux_impl/mod.rs
@@ -1,13 +1,5 @@
 use super::{Error, Stdio};
 
-use std::ffi::OsStr;
-use std::path;
-
-use tempfile::TempDir;
-
-use tokio::runtime;
-
-use openssh_mux_client::connection::Connection;
 pub(crate) use openssh_mux_client::connection::{ForwardType, Socket};
 
 mod stdio;
@@ -20,85 +12,5 @@ pub(crate) use command::Command;
 mod child;
 pub(crate) use child::RemoteChild;
 
-#[derive(Debug)]
-pub(crate) struct Session {
-    /// TempDir will automatically removes the temporary dir on drop
-    tempdir: Option<TempDir>,
-}
-
-// TODO: UserKnownHostsFile for custom known host fingerprint.
-// TODO: Extract process output in Session::check(), Session::connect(), and Session::terminate().
-
-impl Session {
-    pub(crate) fn new(dir: TempDir) -> Self {
-        Self { tempdir: Some(dir) }
-    }
-
-    fn ctl(&self) -> path::PathBuf {
-        self.tempdir.as_ref().unwrap().path().join("master")
-    }
-
-    pub(crate) async fn check(&self) -> Result<(), Error> {
-        Connection::connect(&self.ctl())
-            .await?
-            .send_alive_check()
-            .await?;
-
-        Ok(())
-    }
-
-    pub(crate) fn raw_command<S: AsRef<OsStr>>(&self, program: S) -> Command {
-        let program = program.as_ref().to_string_lossy();
-        Command::new(self.ctl(), program.to_string())
-    }
-
-    pub(crate) async fn request_port_forward(
-        &self,
-        forward_type: ForwardType,
-        listen_socket: &Socket<'_>,
-        connect_socket: &Socket<'_>,
-    ) -> Result<(), Error> {
-        Connection::connect(&self.ctl())
-            .await?
-            .request_port_forward(forward_type, listen_socket, connect_socket)
-            .await?;
-
-        Ok(())
-    }
-
-    async fn request_server_shutdown(tempdir: &TempDir) -> Result<(), Error> {
-        Connection::connect(&tempdir.path().join("master"))
-            .await?
-            .request_stop_listening()
-            .await?;
-
-        Ok(())
-    }
-
-    pub(crate) async fn close(mut self) -> Result<(), Error> {
-        // This also set self.tempdir to None so that Drop::drop would do nothing.
-        let tempdir = self.tempdir.take().unwrap();
-
-        Self::request_server_shutdown(&tempdir).await?;
-
-        tempdir.close().map_err(Error::Cleanup)?;
-
-        Ok(())
-    }
-}
-
-impl Drop for Session {
-    fn drop(&mut self) {
-        // Keep tempdir alive until the connection is established
-        let tempdir = match self.tempdir.take() {
-            Some(tempdir) => tempdir,
-            None => return,
-        };
-
-        if let Ok(handle) = runtime::Handle::try_current() {
-            handle.spawn(async move {
-                let _ = Self::request_server_shutdown(&tempdir).await;
-            });
-        }
-    }
-}
+mod session;
+pub(crate) use session::Session;
