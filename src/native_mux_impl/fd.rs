f35b49ecbf6c20e605f44fab54820a99e8b83c36
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-24 18:23:43 +1100

    Rewrite get_null_fd: Use `get_or_try_init`
    This commit fixed possibility of panic on error path;
    
    It would also retry opening `/dev/null` again if it failed to open, as
    opening file could file because ENOMEM and ENFILE.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/native_mux_impl/fd.rs
+++ src/native_mux_impl/fd.rs
@@ -1,8 +1,6 @@
 use super::Error;
 
-use std::fs::File;
-use std::fs::OpenOptions;
-use std::io;
+use std::fs::{File, OpenOptions};
 
 use std::os::unix::io::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
 
@@ -10,24 +8,16 @@ use once_cell::sync::OnceCell;
 
 /// Open "/dev/null" with RW.
 fn get_null_fd() -> Result<RawFd, Error> {
-    let err_msg =
-        "std::fs::OpenOptions returns error that is not construct using Error::last_os_error";
-    static NULL_FD: OnceCell<Result<File, i32>> = OnceCell::new();
-    let res = NULL_FD.get_or_init(|| {
+    static NULL_FD: OnceCell<File> = OnceCell::new();
+    let res = NULL_FD.get_or_try_init(|| {
         OpenOptions::new()
             .read(true)
             .write(true)
             .open("/dev/null")
-            .map_err(|err| err.raw_os_error().expect(err_msg))
+            .map_err(Error::ChildIo)
     });
 
-    match res {
-        Ok(f) => Ok(AsRawFd::as_raw_fd(f)),
-        Err(err_code) => {
-            let io_err = io::Error::from_raw_os_error(*err_code);
-            Err(Error::ChildIo(io_err))
-        }
-    }
+    res.map(AsRawFd::as_raw_fd)
 }
 
 pub(crate) fn into_fd<T: IntoRawFd>(val: T) -> File {
