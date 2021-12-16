078c3be3c10bc54d12da80470e13d0c155ebf17b
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-16 21:18:27 +1100

    Impl `Fd::get_access_mode` and `AccessMode`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/fd.rs
+++ src/fd.rs
@@ -41,6 +41,18 @@ impl Fd {
         // safety: self.0 is guaranteed to contain a valid fd.
         unsafe { dup(self.0) }
     }
+
+    #[cfg(feature = "native-mux")]
+    pub(crate) fn get_access_mode(&self) -> Result<AccessMode, Error> {
+        // safety: self.0 is guaranteed to contain a valid fd.
+        let res = unsafe { libc::fcntl(self.0, libc::F_GETFL) };
+
+        if res == -1 {
+            Err(Error::ChildIo(io::Error::last_os_error()))
+        } else {
+            Ok(AccessMode(res & libc::O_ACCMODE))
+        }
+    }
 }
 
 impl Drop for Fd {
@@ -54,5 +66,37 @@ impl Drop for Fd {
             self.0,
             io::Error::last_os_error()
         );
+    }
+}
+
+#[cfg(feature = "native-mux")]
+#[derive(Debug, Copy, Clone)]
+pub(crate) struct AccessMode(libc::c_int);
+
+#[cfg(feature = "native-mux")]
+impl AccessMode {
+    /// Return true if the fd can only be read.
+    pub(crate) const fn is_rdonly(&self) -> bool {
+        self.0 == libc::O_RDONLY
+    }
+
+    /// Return true if the fd can only be write.
+    pub(crate) const fn is_wronly(&self) -> bool {
+        self.0 == libc::O_WRONLY
+    }
+
+    /// Return true if the fd is readable and writeable.
+    pub(crate) const fn is_rdwr(&self) -> bool {
+        self.0 == libc::O_RDWR
+    }
+
+    /// Return true if the fd can be read.
+    pub(crate) const fn is_readable(&self) -> bool {
+        self.is_rdonly() || self.is_rdwr()
+    }
+
+    /// Return true if the fd can be write.
+    pub(crate) const fn is_writeable(&self) -> bool {
+        self.is_wronly() || self.is_rdwr()
     }
 }
