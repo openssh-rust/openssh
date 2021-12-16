ee018482cd3f67c46d5bb90bcdf5892d442703a3
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-16 17:17:45 +1100

    update doc on `Session::connect_mux` to match `SessionBuilder::connect_mux`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -241,7 +241,7 @@ impl Session {
     /// Connect to the host at the given `host` over SSH using native mux impl, which
     /// will create a new socket connection for each `Child` created.
     ///
-    /// **PLEASE READ THE CRATE-LEVEL DOCUMENTATION FOR DETAILS**.
+    /// See the crate-level documentation for more details on the difference between native and process-based mux.
     ///
     /// The format of `destination` is the same as the `destination` argument to `ssh`. It may be
     /// specified as either `[user@]hostname` or a URI of the form `ssh://[user@]hostname[:port]`.
