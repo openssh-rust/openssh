602f252f3fab0478530fc21368b16159595f4204
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-16 22:05:41 +1100

    Simplify `native_mux_impl::RemoteChild` impl
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/native_mux_impl/child.rs
+++ src/native_mux_impl/child.rs
@@ -1,42 +1,14 @@
 use super::{ChildStderr, ChildStdin, ChildStdout, Error};
 
-use std::mem::replace;
-
 use std::io;
 use std::os::unix::process::ExitStatusExt;
 use std::process::ExitStatus;
 
 use openssh_mux_client::{EstablishedSession, SessionStatus};
 
-macro_rules! do_wait {
-    ($state:expr, $var:ident, $then:block) => {{
-        let state = $state;
-
-        let exit_value = match replace(state, RemoteChildState::AwaitingExit) {
-            RemoteChildState::AwaitingExit => unreachable!(),
-            RemoteChildState::Exited(exit_value) => exit_value,
-            RemoteChildState::Running($var) => $then,
-        };
-
-        *state = RemoteChildState::Exited(exit_value);
-        if let Some(val) = exit_value {
-            if val == 127 {
-                Err(Error::Remote(io::Error::new(
-                    io::ErrorKind::NotFound,
-                    "remote command not found",
-                )))
-            } else {
-                Ok(ExitStatusExt::from_raw((val as i32) << 8))
-            }
-        } else {
-            Err(Error::RemoteProcessTerminated)
-        }
-    }};
-}
-
 #[derive(Debug)]
 pub(crate) struct RemoteChild {
-    state: RemoteChildState,
+    established_session: EstablishedSession,
     child_stdin: Option<ChildStdin>,
     child_stdout: Option<ChildStdout>,
     child_stderr: Option<ChildStderr>,
@@ -50,7 +22,7 @@ impl RemoteChild {
         child_stderr: Option<ChildStderr>,
     ) -> Self {
         Self {
-            state: RemoteChildState::Running(established_session),
+            established_session,
             child_stdin,
             child_stdout,
             child_stderr,
@@ -61,21 +33,32 @@ impl RemoteChild {
         Ok(())
     }
 
-    pub(crate) async fn wait(mut self) -> Result<ExitStatus, Error> {
-        do_wait!(&mut self.state, established_session, {
-            match established_session.wait().await {
-                Ok(session_status) => match session_status {
-                    SessionStatus::TtyAllocFail(_established_session) => {
-                        unreachable!("mux_client_impl never allocates a tty")
+    pub(crate) async fn wait(self) -> Result<ExitStatus, Error> {
+        let session_status = self
+            .established_session
+            .wait()
+            .await
+            .map_err(|(err, _established_session)| err)?;
+
+        match session_status {
+            SessionStatus::TtyAllocFail(_established_session) => {
+                unreachable!("native_mux_impl never allocates a tty")
+            }
+            SessionStatus::Exited { exit_value } => {
+                if let Some(val) = exit_value {
+                    if val == 127 {
+                        Err(Error::Remote(io::Error::new(
+                            io::ErrorKind::NotFound,
+                            "remote command not found",
+                        )))
+                    } else {
+                        Ok(ExitStatusExt::from_raw((val as i32) << 8))
                     }
-                    SessionStatus::Exited { exit_value } => exit_value,
-                },
-                Err((err, established_session)) => {
-                    self.state = RemoteChildState::Running(established_session);
-                    return Err(err.into());
+                } else {
+                    Err(Error::RemoteProcessTerminated)
                 }
             }
-        })
+        }
     }
 
     pub(crate) fn stdin(&mut self) -> &mut Option<ChildStdin> {
@@ -89,13 +72,4 @@ impl RemoteChild {
     pub(crate) fn stderr(&mut self) -> &mut Option<ChildStderr> {
         &mut self.child_stderr
     }
-}
-
-#[derive(Debug)]
-enum RemoteChildState {
-    Running(EstablishedSession),
-    Exited(Option<u32>),
-
-    /// The function wait is being called.
-    AwaitingExit,
 }
