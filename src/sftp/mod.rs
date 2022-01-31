0bfd61a4871f57c77023cbe648a0719f553088b1
Jiahao XU <Jiahao_XU@outlook.com>
2022-01-31 17:41:41 +1100

    Rename `Sftp::options()` to `Sftp::open_options()`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/sftp/mod.rs
+++ src/sftp/mod.rs
@@ -152,7 +152,7 @@ impl<'s> Sftp<'s> {
         self.thread_local_cache.get_or(|| Cache::new(None)).set(id);
     }
 
-    pub fn options(&self) -> OpenOptions<'_, '_> {
+    pub fn open_options(&self) -> OpenOptions<'_, '_> {
         OpenOptions::new(self)
     }
 }
