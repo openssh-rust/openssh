aecefdb446d8f9cc454110f8be70cab9bfb8f2d6
Jiahao XU <Jiahao_XU@outlook.com>
2022-02-03 15:14:52 +1100

    Avoid rebuild of deps in run_ci_tests.sh
    by using a different targetdir.
    
    Use a different target dir since `RUSTFLAGS='--cfg=ci'`
    seems to affect not just the crate itself, but also
    the dependencies as well.
    
    Since IDEs usually do not set RUSTFLAGS='--cfg=ci',
    setting it in `run_ci_tests.sh` would cause
    all the dependencies and openssh to be rebuilt.
    
    Thus it makes run_ci_tests.sh incrediably slow,
    and it also affects IDEs checking, since now
    the IDEs also need to rebuild the crate.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -30,9 +30,20 @@ trap cleanup EXIT
 
 export RUSTFLAGS='--cfg=ci'
 
-cargo hack check --feature-powerset
+# Use a different target-dir since RUSTFLAGS='--cfg=ci' seems to
+# affect dependencies as well.
+#
+# Since IDEs usually do not set RUSTFLAGS='--cfg=ci', setting it
+# here would cause all the dependencies and openssh to be rebuilt.
+#
+# Thus it makes run_ci_tests.sh incrediably slow, and it also
+# affects IDEs checking, since now the IDEs also need to
+# rebuild the crate.
+CARGO_OPTS='--target-dir ci-target'
 
-cargo clippy --all-features
+cargo hack --feature-powerset check $CARGO_OPTS
+
+cargo clippy --all-features $CARGO_OPTS
 
 export HOSTNAME=127.0.0.1
 chmod 600 .test-key
@@ -51,6 +62,7 @@ rm -rf control-test config-file-test .ssh-connection*
 
 echo Running integration test
 cargo test \
+    $CARGO_OPTS \
     --all-features \
     --no-fail-fast \
     -- --test-threads=3 # Use test-threads=3 so that the output is readable
