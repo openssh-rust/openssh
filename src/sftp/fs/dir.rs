d675770d4b3bb72ca7d5c62eaac86480b39cff8d
Jiahao XU <Jiahao_XU@outlook.com>
2022-02-04 23:06:54 +1100

    Impl `ReadDir::{as_(mut_)slice, into_inner}`
    and `IntoIterator` for `ReadDir`.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/sftp/fs/dir.rs
+++ src/sftp/fs/dir.rs
@@ -2,6 +2,8 @@ use super::super::{FileType, MetaData};
 
 use openssh_sftp_client::NameEntry;
 use std::path::Path;
+use std::slice::{Iter, IterMut};
+use std::vec::IntoIter;
 
 /// Dir entry
 #[repr(transparent)]
@@ -34,3 +36,59 @@ impl DirEntry {
 #[repr(transparent)]
 #[derive(Debug, Clone)]
 pub struct ReadDir(pub(super) Box<[NameEntry]>);
+
+impl ReadDir {
+    /// Return slice of [`DirEntry`]s.
+    pub fn as_slice(&self) -> &[DirEntry] {
+        let entries: &[NameEntry] = &*self.0;
+        let ptr = entries as *const [NameEntry] as *const [DirEntry];
+
+        // Safety: DirEntry is transparent
+        unsafe { &*ptr }
+    }
+
+    /// Return mutable slice of [`DirEntry`]s.
+    pub fn as_mut_slice(&mut self) -> &mut [DirEntry] {
+        let entries: &mut [NameEntry] = &mut *self.0;
+        let ptr = entries as *mut [NameEntry] as *mut [DirEntry];
+
+        // Safety: DirEntry is transparent
+        unsafe { &mut *ptr }
+    }
+
+    /// Return boxed slice of [`DirEntry`]s.
+    pub fn into_inner(self) -> Box<[DirEntry]> {
+        let ptr = Box::into_raw(self.0);
+
+        // Safety: DirEntry is transparent
+        unsafe { Box::from_raw(ptr as *mut [DirEntry]) }
+    }
+}
+
+impl<'a> IntoIterator for &'a ReadDir {
+    type Item = &'a DirEntry;
+    type IntoIter = Iter<'a, DirEntry>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.as_slice().iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a mut ReadDir {
+    type Item = &'a mut DirEntry;
+    type IntoIter = IterMut<'a, DirEntry>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.as_mut_slice().iter_mut()
+    }
+}
+
+impl IntoIterator for ReadDir {
+    type Item = DirEntry;
+    type IntoIter = IntoIter<DirEntry>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let vec: Vec<DirEntry> = self.into_inner().into();
+        vec.into_iter()
+    }
+}
