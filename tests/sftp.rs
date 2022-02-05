607f0fec3f149943e50820eae7acf3028aa4be0e
Jiahao XU <Jiahao_XU@outlook.com>
2022-02-05 19:38:43 +1100

    Refactor integration tests: Extract mod `common`
    for shared code between integration test openssh and sftp.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- tests/sftp.rs
+++ tests/sftp.rs
@@ -1,28 +1,7 @@
+mod common;
+use common::*;
+
 use openssh::*;
-
-fn addr() -> String {
-    std::env::var("TEST_HOST").unwrap_or("ssh://test-user@127.0.0.1:2222".to_string())
-}
-
-async fn connects() -> Vec<Session> {
-    let mut sessions = Vec::with_capacity(2);
-
-    #[cfg(feature = "process-mux")]
-    {
-        sessions.push(Session::connect(&addr(), KnownHosts::Accept).await.unwrap());
-    }
-
-    #[cfg(feature = "native-mux")]
-    {
-        sessions.push(
-            Session::connect_mux(&addr(), KnownHosts::Accept)
-                .await
-                .unwrap(),
-        );
-    }
-
-    sessions
-}
 
 #[tokio::test]
 #[cfg_attr(not(ci), ignore)]
