98693eb22cd1ead7377c3e32e4d7434fa5f931f5
Jiahao XU <Jiahao_XU@outlook.com>
2022-02-06 22:17:19 +1100

    Add integration test `sftp_file_write_all`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- tests/sftp.rs
+++ tests/sftp.rs
@@ -3,8 +3,11 @@ use common::*;
 
 use openssh::sftp::*;
 
-use std::cmp::min;
+use std::cmp::{max, min};
 use std::path::Path;
+
+use bytes::BytesMut;
+use tokio::io::AsyncSeekExt;
 
 #[tokio::test]
 #[cfg_attr(not(ci), ignore)]
@@ -59,6 +62,49 @@ async fn sftp_file_basics() {
             );
 
             debug_assert_eq!(&*fs.read(path).await.unwrap(), &*content);
+
+            // remove the file
+            fs.remove_file(path).await.unwrap();
+        }
+
+        // close sftp and session
+        sftp.close().await.unwrap();
+        session.close().await.unwrap();
+    }
+}
+
+#[tokio::test]
+#[cfg_attr(not(ci), ignore)]
+/// Test File::write_all, File::read_all and AsyncSeek implementation
+async fn sftp_file_write_all() {
+    let path = "/tmp/sftp_file_write_all";
+
+    for session in connects().await {
+        let sftp = session.sftp(SftpOptions::new()).await.unwrap();
+
+        let max_len = max(sftp.max_write_len(), sftp.max_read_len()) as usize;
+        let content = b"HELLO, WORLD!\n".repeat(max_len / 8);
+
+        {
+            let mut file = sftp
+                .options()
+                .write(true)
+                .read(true)
+                .create(true)
+                .open(path)
+                .await
+                .unwrap();
+            let mut fs = sftp.fs(Some(""));
+
+            file.write_all(&content).await.unwrap();
+            file.rewind().await.unwrap();
+
+            let buffer = file
+                .read_all(content.len(), BytesMut::with_capacity(content.len()))
+                .await
+                .unwrap();
+
+            assert_eq!(&*buffer, &*content);
 
             // remove the file
             fs.remove_file(path).await.unwrap();
