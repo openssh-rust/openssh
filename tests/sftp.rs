2feb371d088d1b93e48a147d7af810d5f86743c9
Jiahao XU <Jiahao_XU@outlook.com>
2022-02-08 16:12:42 +1100

    Add integration test `sftp_tokio_compact_file_write_all`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- tests/sftp.rs
+++ tests/sftp.rs
@@ -279,6 +279,83 @@ async fn sftp_tokio_compact_file_basics() {
     }
 }
 
+struct SftpTokioCompactFileWriteAllTester<'s> {
+    path: &'static Path,
+    file: TokioCompactFile<'s>,
+    fs: Fs<'s>,
+    content: Vec<u8>,
+}
+
+impl<'s> SftpTokioCompactFileWriteAllTester<'s> {
+    async fn new(
+        sftp: &'s Sftp<'s>,
+        path: &'static Path,
+    ) -> SftpTokioCompactFileWriteAllTester<'s> {
+        let max_len = max(sftp.max_write_len(), sftp.max_read_len()) as usize;
+        let content = b"HELLO, WORLD!\n".repeat(max_len / 8);
+
+        let file = sftp
+            .options()
+            .write(true)
+            .read(true)
+            .create(true)
+            .open(path)
+            .await
+            .map(TokioCompactFile::new)
+            .unwrap();
+        let fs = sftp.fs("");
+
+        Self {
+            path,
+            file,
+            fs,
+            content,
+        }
+    }
+
+    async fn assert_content(mut self) {
+        let len = self.content.len();
+
+        self.file.rewind().await.unwrap();
+
+        let buffer = self
+            .file
+            .read_all(len, BytesMut::with_capacity(len))
+            .await
+            .unwrap();
+
+        assert_eq!(&*buffer, &*self.content);
+
+        // remove the file
+        self.fs.remove_file(self.path).await.unwrap();
+    }
+}
+
+#[tokio::test]
+#[cfg_attr(not(ci), ignore)]
+/// Test File::write_all_vectorized, File::read_all and AsyncSeek implementation
+async fn sftp_tokio_compact_file_write_all() {
+    let path = Path::new("/tmp/sftp_tokio_compact_file_write_all");
+
+    for session in connects().await {
+        let sftp = session
+            .sftp(SftpOptions::new().max_write_len(200).max_read_len(200))
+            .await
+            .unwrap();
+
+        let mut tester = SftpTokioCompactFileWriteAllTester::new(&sftp, path).await;
+
+        let content = &tester.content;
+
+        tester.file.write_all(&content).await.unwrap();
+        tester.assert_content().await;
+
+        // close sftp and session
+        sftp.close().await.unwrap();
+        session.close().await.unwrap();
+    }
+}
+
 #[tokio::test]
 #[cfg_attr(not(ci), ignore)]
 /// Test creating, removing and iterating over dir, as well
