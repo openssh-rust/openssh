96f4f6bd2b538fdc71c94e1f1184a72c42757602
Jiahao XU <Jiahao_XU@outlook.com>
2022-04-14 16:14:30 +1000

    FIx `{start, stop}_sshd.sh`: Do not mod `$XDG_RUNTIME_DIR`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- stop_sshd.sh
+++ stop_sshd.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 
 cd "$(dirname "$(realpath "$0")")"
 
-[ -z ${XDG_RUNTIME_DIR+x} ] && export XDG_RUNTIME_DIR=/tmp
+export RUNTIME_DIR=${XDG_RUNTIME_DIR:-/tmp}
 
-rm "$XDG_RUNTIME_DIR/openssh-rs/sshd_started"
-rm "$XDG_RUNTIME_DIR/openssh-rs/known_hosts"
+rm "$RUNTIME_DIR/openssh-rs/sshd_started"
+rm "$RUNTIME_DIR/openssh-rs/known_hosts"
 
 docker stop openssh
