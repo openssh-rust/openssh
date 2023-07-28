fefd900437bcab40be0a0f10bc49712cf1f29b16
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-07-28 03:10:16 -0500

    Allow conversion of `std::process::Command` into a `openssh::Command` given a `openssh::Session`. (#112)
    * Add OverSsh trait to allow conversion of std::process::Command into a openssh::Session.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Add a doc comment and example usage.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Add tests for OverSsh's with_session
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Use raw_command and raw_args. Refactor the name to 'over_ssh'.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Remove a dangling test import. Use better name for test.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Clean up doc comment.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Apply suggestions of code review.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Update src/command.rs
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * Apply more suggestions from the code review.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Apply more suggestions from the code review.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Update src/command.rs
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * Update src/lib.rs
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * Update src/escape.rs
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * Apply more suggestions. Add a test for non-utf8 chars for escape.
    
    * Slight refactor of escape.
    
    Signed-off-by: Aalekh Patel <aalekh@protectchildren.ca>
    
    * Run cargo clippy --fix and persist all modifications to escape.rs
    
    Signed-off-by: Aalekh Patel <aalekh@protectchildren.ca>
    
    * Apply rustfmt to command.rs and escape.rs
    
    Signed-off-by: Aalekh Patel <aalekh@protectchildren.ca>
    
    * Lint over_session test
    
    Signed-off-by: Aalekh Patel <aalekh@protectchildren.ca>
    
    * Update src/escape.rs
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * Update src/escape.rs
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * Update src/escape.rs
    
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    
    * Apply more suggestions.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Rustfmt fix.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Fix a doctest for overssh.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Add a test for overssh that requires escaping the argument passed to the command.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Update overssh require-escaping-arguments test to contain a space.
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    * Apply rustfmt to tests/openssh.rs
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    
    ---------
    
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
    Signed-off-by: Aalekh Patel <aalekh@protectchildren.ca>
    Co-authored-by: Jiahao XU <Jiahao_XU@outlook.com>
    Co-authored-by: Aalekh Patel <aalekh@protectchildren.ca>

---
+++ src/escape.rs
@@ -0,0 +1,91 @@
+//! Escape characters that may have special meaning in a shell, including spaces.
+//! This is a modified version of the [`shell-escape::unix`] module of [`shell-escape`] crate.
+//!
+//! [`shell-escape`]: https://crates.io/crates/shell-escape
+//! [`shell-escape::unix`]: https://docs.rs/shell-escape/latest/src/shell_escape/lib.rs.html#101
+
+use std::{
+    borrow::Cow,
+    ffi::{OsStr, OsString},
+    os::unix::ffi::OsStrExt,
+    os::unix::ffi::OsStringExt,
+};
+
+fn allowed(byte: u8) -> bool {
+    matches!(byte, b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' | b'-' | b'_' | b'=' | b'/' | b',' | b'.' | b'+')
+}
+
+/// Escape characters that may have special meaning in a shell, including spaces.
+///
+/// **Note**: This function is an adaptation of [`shell-escape::unix::escape`].
+/// This function exists only for type compatibility and the implementation is
+/// almost exactly the same as [`shell-escape::unix::escape`].
+///
+/// [`shell-escape::unix::escape`]: https://docs.rs/shell-escape/latest/src/shell_escape/lib.rs.html#101
+///
+pub(crate) fn escape(s: &OsStr) -> Cow<'_, OsStr> {
+    let as_bytes = s.as_bytes();
+    let all_allowed = as_bytes.iter().copied().all(allowed);
+
+    if !as_bytes.is_empty() && all_allowed {
+        return Cow::Borrowed(s);
+    }
+
+    let mut escaped = Vec::with_capacity(as_bytes.len() + 2);
+    escaped.push(b'\'');
+
+    for &b in as_bytes {
+        match b {
+            b'\'' | b'!' => {
+                escaped.reserve(4);
+                escaped.push(b'\'');
+                escaped.push(b'\\');
+                escaped.push(b);
+                escaped.push(b'\'');
+            }
+            _ => escaped.push(b),
+        }
+    }
+    escaped.push(b'\'');
+    OsString::from_vec(escaped).into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_escape_case(input: &str, expected: &str) {
+        test_escape_from_bytes(input.as_bytes(), expected.as_bytes())
+    }
+
+    fn test_escape_from_bytes(input: &[u8], expected: &[u8]) {
+        let input_os_str = OsStr::from_bytes(input);
+        let observed_os_str = escape(input_os_str);
+        let expected_os_str = OsStr::from_bytes(expected);
+        assert_eq!(observed_os_str, expected_os_str);
+    }
+
+    // These tests are courtesy of the `shell-escape` crate.
+    #[test]
+    fn test_escape() {
+        test_escape_case(
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_=/,.+",
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_=/,.+",
+        );
+        test_escape_case("--aaa=bbb-ccc", "--aaa=bbb-ccc");
+        test_escape_case(
+            "linker=gcc -L/foo -Wl,bar",
+            r#"'linker=gcc -L/foo -Wl,bar'"#,
+        );
+        test_escape_case(r#"--features="default""#, r#"'--features="default"'"#);
+        test_escape_case(r#"'!\$`\\\n "#, r#"''\'''\!'\$`\\\n '"#);
+        test_escape_case("", r#"''"#);
+        test_escape_case(" ", r#"' '"#);
+        test_escape_case("*", r#"'*'"#);
+
+        test_escape_from_bytes(
+            &[0x66, 0x6f, 0x80, 0x6f],
+            &[b'\'', 0x66, 0x6f, 0x80, 0x6f, b'\''],
+        );
+    }
+}
